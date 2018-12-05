e77f1cb758181a14368703ca8809f12c05e765ba
haha479 <283275935@qq.com>
2018-12-05 11:33:28 +0800

    更换唯一字符码

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Hh321123@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["SECRET_KEY"] = 'af2fad8cfe1f4c5fac4aa5edf6fcc8f3'
+app.config["SECRET_KEY"] = 'cd13414d562a4f638c11982d87edc823'
 app.config["REDIS_URL"] = "redis://localhost:6379/0"
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
 app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users/")
