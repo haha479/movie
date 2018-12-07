891d2e84aa0d2f76e2518628b0ae167b18ae2cf5
haha479 <283275935@qq.com>
2018-12-07 17:30:28 +0800

    管理员-管理员用户修改密码

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,11 +1,12 @@
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request
-from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
+from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm, PwdForm
 from app.models import Admin, Tag, Movie, Preview, User, Comment, Moviecol
 from functools import wraps
 from app import db, app
 import math
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
 import os
 import datetime
 import uuid
@@ -41,9 +42,8 @@ def login():
 		# 根据用户输入的账号查询数据库获取一条账户信息
 		admin = Admin.query.filter_by(name=data["account"]).first()
 		# 调用在models中定义的check_pwd方法查询密码
-		print('haha:', data["pwd"])
 		if not admin.check_pwd(data["pwd"]):
-			flash("密码错误!")
+			flash("密码错误!", 'err')
 			return redirect(url_for("admin.login"))
 		# 将账户账号存入session
 		session["admin"] = data["account"]
@@ -58,10 +58,20 @@ def logout():
 	session.pop("admin", None)
 	return redirect(url_for("admin.login"))
 
-@admin.route("/pwd/")
+# 修改密码
+@admin.route("/pwd/", methods=['GET', 'POST'])
 @admin_login_req
 def pwd():
-	return render_template("admin/pwd.html")
+	form = PwdForm()
+	if form.validate_on_submit():
+		data = form.data
+		admin = Admin.query.filter_by(name=session['admin']).first()
+		admin.pwd = generate_password_hash(data['new_pwd'])
+		db.session.add(admin)
+		db.session.commit()
+		flash('修改密码成功, 请重新登录', 'ok')
+		return redirect(url_for('admin.logout'))
+	return render_template("admin/pwd.html", form=form)
 
 # 添加标签
 @admin.route("/tag/add/", methods=["GET", "POST"])
